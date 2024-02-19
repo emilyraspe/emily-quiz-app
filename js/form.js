15c971e846d42c033152a41af465722c89f3eeee
Emily Raspe <emilyraspe@icloud.com>
2024-02-19 12:00:41 +0100

    fix errors in js

--- js/form.js
+++ js/form.js
@@ -18,8 +18,27 @@ form.addEventListener("submit", (event) => {
   console.log("data: ", data);
 
   //add card
+  const divForCard = document.createElement("div");
   const card = document.createElement("section");
 
-  main.append(card);
+  main.append(divForCard);
+  divForCard.append(card);
+  divForCard.classList.add("content");
   card.classList.add("cards");
+
+  //innerhtml for card
+
+  card.innerHTML = `
+  <i class="fa-solid fa-bookmark cards--icon" data-js="bookmark"></i>
+  <p class="cards--text">
+  ${question}
+  </p>
+
+  <button data-js="answer-button">Show answer</button>
+  <p hidden class="cards--text" data-js="answer">
+  ${answer}
+  </p>
+  <div class="cards--categories--main">
+    <span class="cards--categories">  ${tag} </span>
+  </div>`;
 });
