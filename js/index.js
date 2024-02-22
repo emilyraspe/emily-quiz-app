8c2e0488ec307fbcab196cd800aab2691375ccf0
Emily Raspe <emilyraspe@icloud.com>
2024-02-22 15:44:13 +0100

    edit bookmarks and buttons so the js works across the app

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 // toggle function for bookmark
-const bookmark = document.querySelector('[data-js="bookmark"]');
+const bookmark = document.querySelectorAll(".fa-bookmark");
 
-bookmark.addEventListener("click", () => {
-  if (bookmark.classList.contains("cards--icon--active")) {
-    bookmark.classList.remove("cards--icon--active");
-  } else {
-    bookmark.classList.add("cards--icon--active");
-  }
+bookmark.forEach((bookmark) => {
+  bookmark.addEventListener("click", () => {
+    if (bookmark.classList.contains("cards--icon--active")) {
+      bookmark.classList.remove("cards--icon--active");
+    } else {
+      bookmark.classList.add("cards--icon--active");
+    }
+  });
 });
 
 //toggle function for answer
-const answerButton = document.querySelector('[data-js="answer-button"]');
-const answer = document.querySelector('[data-js="answer"]');
+const answerButton = document.querySelectorAll('[data-js="answer-button"]');
+const answer = document.querySelectorAll('[data-js="answer"]');
 
-answerButton.addEventListener("click", () => {
-  if (answer.hasAttribute("hidden")) {
-    answer.removeAttribute("hidden");
-    answerButton.textContent = "Hide answer";
-  } else {
-    answer.setAttribute("hidden", true);
-    answerButton.textContent = "Show answer";
-  }
+answerButton.forEach((answerButton, index) => {
+  answerButton.addEventListener("click", () => {
+    if (answer[index].hasAttribute("hidden")) {
+      answer[index].removeAttribute("hidden");
+      answerButton.textContent = "Hide answer";
+    } else {
+      answer[index].setAttribute("hidden", true);
+      answerButton.textContent = "Show answer";
+    }
+  });
 });
